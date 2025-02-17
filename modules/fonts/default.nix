6abaf4af0205407ea7a5153177bf12b0a351d836
Moskas <minemoskas@gmail.com>
2025-02-17 08:41:35 +0100

    chore: flake bump

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   terminal.font = let
-    jetbrains = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+    jetbrains = pkgs.nerd-fonts.jetbrains-mono; #pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
     fontPath =
       "share/fonts/truetype/NerdFonts/JetBrainsMonoNerdFontMono-Regular.ttf";
   in "${jetbrains}/${fontPath}";
